a8731df33ce810e88b744a440d411b96c7c60727
hansemannn <hansemannn@users.noreply.github.com>
2016-10-26 23:01:03 +0200

    Support detail images

--- app/alloy.js
+++ app/alloy.js
@@ -1,3 +1,4 @@
 Alloy.Collections.lunch = Alloy.createCollection("lunch");
 Alloy.Models.cart = Alloy.createModel("cart"); 
 Alloy.Globals.Map = require("ti.map");
+Alloy.Globals.displayWidth = Ti.Platform.getDisplayCaps().getPlatformWidth();
--- app/controllers/lunches/details.js
+++ app/controllers/lunches/details.js
@@ -16,7 +16,63 @@ var nav,
 })(arguments[0] || {});
 
 function setUI() {
+	$.title.setText(product.name);
+	setImages();
+}
 
+function setImages() {
+	var images = product.images || null;
+	var views = [];
+	
+	if (images.length < 2) {
+		$.images.setShowPagingControl(false);
+	}
+	
+	if (!images || images.length == 0) {
+		return;
+	}
+	
+	$.images.removeAllChildren();
+	$.placeholder.hide();
+	
+	// TODO: Move to Alloy-based generation
+		
+	_.each(images, function(image) {
+		var view = Ti.UI.createView({
+			height: Ti.UI.SIZE,
+			width: Ti.UI.SIZE,
+			left: 15,
+			top: 10,
+			height: 165,
+			width: 165,
+			borderRadius: 0,
+			viewShadowRadius: 2,
+			viewShadowColor: "#A6444444", // 65 %
+			viewShadowOffset: {x: 0, y: 2}
+		});
+		view.add(Ti.UI.createImageView({
+			defaultImage: "/images/noImage.png",
+			borderRadius: 0,
+			image: image,
+		}));
+		$.images.add(view);
+	});
+	 	
+	var label = Ti.UI.createLabel({
+		text: "+ ",
+		color: "#fff",
+		opacity: 0.3,
+		top: 40,
+		left: 10,
+		font: {
+			fontSize: 80,
+			fontWeight: "bold"
+		}
+	});
+	
+	label.addEventListener("click", showCamera);	
+		
+	$.images.add(label);
 }
 
 function close() {
@@ -34,3 +90,7 @@ exports.open = function(animated) {
         $.details.open();
     }
 };
+
+function showCamera(e) {
+	
+}
