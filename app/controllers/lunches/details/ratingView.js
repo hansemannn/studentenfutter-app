815b2c76d7a384e1769e0fcc73acea8ffce0658d
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-01-15 00:01:20 +0100

    Finish rating UI

--- app/controllers/lunches/details/ratingView.js
+++ app/controllers/lunches/details/ratingView.js
@@ -1,5 +1,6 @@
 var parent,
-	productId;
+	productId,
+	rating;
 
 /**
  *  Constructor
@@ -7,9 +8,39 @@ var parent,
 (function constructor(args) {	
 	parent = args.parent;	
 	productId = args.productId;
+	
+	generateRatingUI();
 })(arguments[0] || {});
 
-function setRating() {
+function setRating(e) {
+	rating = e.source.userData ? e.source.userData.value : null;
+	
+	if (!rating) {
+		return;
+	}
+		
+	// TODO: Find smarter way for this midnight hacking!
+	
+	for (var i = 1; i <= rating; i++) {
+		$.rating.children[i - 1].setImage('/images/icons/singleStarFull.png');
+	}
+	
+	if (rating < 5) {
+		for (var i = rating + 1; i <= 5; i++) {
+			$.rating.children[i - 1].setImage('/images/icons/singleStarEmpty.png');
+		} 	
+	}	
+
+	$.cancel.setEnabled(true);
+	$.submit.setEnabled(true);
+}
+
+function submitRating() {
+	if (!rating) {
+		Ti.API.error('State error: Should have selected at least 1 at this point!');
+		return;
+	}
+	
 	var LoaderInstance = require('/loader');
 	var api = require('/api');
 	
@@ -18,16 +49,10 @@ function setRating() {
 	
 	api.postRating({
 		productId: productId,
-		value: 5
+		value: rating
 	}, function(e) {		
-		if (e.success) {
-			$.rating.setImage('/images/icons/successCheck.png');			
-		} else {
-			$.rating.setImage('/images/icons/errorCross.png');			
-		}	
-		
 		loader.hide();
-		setTimeout(hide, 1000);	
+		hide();
 	});
 }
 
@@ -40,11 +65,29 @@ function hide() {
 	});
 }
 
+function generateRatingUI() {
+	for (var i = 1; i <= 5; i++) {
+		$.rating.add(Ti.UI.createImageView({
+			image: '/images/icons/singleStarEmpty.png',
+			userData: {
+				value: i
+			},
+			height: 30,
+			width: 30
+		}));
+	}
+}
+
 exports.show = function() {
 	parent.add($.container);
 
 	$.container.animate({
 		opacity: 1,
+		duration: 500
+	});
+	
+	$.content.animate({
+		height:135, 
 		duration: 500
 	});
 };
