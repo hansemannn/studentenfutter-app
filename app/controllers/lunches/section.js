ba2688207e3cedfc46b0d70cf8afd97b7799b50b
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-01-16 18:24:32 +0100

    Apply fix to keep first section in proportion

--- app/controllers/lunches/section.js
+++ app/controllers/lunches/section.js
@@ -3,4 +3,9 @@
  */
 (function constructor(args) {
     $.title.setText(L(args.title.replace(" ","_"), args.title).toUpperCase());
+    
+    // Hacky fix for native grouped tableview to keep first section in format
+    if (OS_IOS && args.index === 0) {
+        $.container.setHeight(25);
+    }
 })(arguments[0] || {});
