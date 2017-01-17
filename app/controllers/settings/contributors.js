0504adb37cf7f9f0619130ae60f88ceac435c50f
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-01-17 23:33:54 +0100

    Add click event to contributors page

--- app/controllers/settings/contributors.js
+++ app/controllers/settings/contributors.js
@@ -19,7 +19,8 @@ function populateContributors() {
             items.push({
                 properties: {
                     itemId: contributor.id,
-                    template: 'contrib'
+                    username: contributor.login,
+                    accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE 
                 },
                 avatar: {
                     image: contributor.avatar_url
@@ -36,4 +37,26 @@ function populateContributors() {
         $.section.setItems(items);
         loader.hide();
     });
+}
+
+function showContributorPage(e) {
+    var item = e.section.getItemAt(e.itemIndex);
+    var username = item.properties.username;
+    var githubURL = 'https://github.com/' + username;
+    
+    if (OS_ANDROID) {
+        Ti.Platform.openURL(githubURL);
+    } else {    
+        var safariDialog = require('ti.safaridialog');
+        
+        if (!safariDialog.isSupported()) {
+            Ti.Platform.openURL(githubURL);
+        } else {
+            safariDialog.open({
+                url: githubURL,
+                tintColor: Alloy.CFG.styles.tintColor,
+                animted: true
+            });    
+        }        
+    }
 }
