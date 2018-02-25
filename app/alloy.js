0fdadd7f0bd809479165105a4ff227c415541f47
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-02-25 15:04:03 +0100

    Move Android to SDK 7

--- app/alloy.js
+++ app/alloy.js
@@ -1,25 +1,32 @@
 import ActionBarHelper from 'actionbar';
 import Map from 'ti.map';
 
-Ti.UI.setBackgroundColor('#fff');
-
 const isiPhoneX = (Ti.Platform.displayCaps.platformWidth === 375 && Ti.Platform.displayCaps.platformHeight === 812 && Ti.Platform.displayCaps.logicalDensityFactor === 3);
+
+let PlayServices = null;
+
+if (OS_ANDROID) {
+	PlayServices = require('ti.playservices');
+}
+
+Ti.UI.setBackgroundColor('#fff');
 
 Alloy.Globals.footerBarHeight = (OS_IOS && Ti.Platform.displayCaps.platformHeight === 812) ? 64 : 44;
 Alloy.Collections.lunch = Alloy.createCollection('lunch');
 Alloy.Models.cart = Alloy.createModel('cart'); 
 Alloy.Globals.displayWidth = Ti.Platform.getDisplayCaps().getPlatformWidth();
-Alloy.Globals.isGooglePlayServicesAvailable = OS_IOS || Map.isGooglePlayServicesAvailable;
+Alloy.Globals.isGooglePlayServicesAvailable = PlayServices && PlayServices.isGooglePlayServicesAvailable();
 Alloy.Globals.footerHeight = isiPhoneX ? 64 : 44;
 
 Alloy.Globals.setAndroidBackButton = function(_window) {
 	if (!OS_ANDROID) { return; }
 
 	_window.addEventListener('open', () => {
-		const actionBarHelper = new ActionBarHelper(window);
+		const actionBarHelper = new ActionBarHelper(_window);
 
-		if (_window.title && _window.title.length > 0)
+		if (_window.title && _window.title.length > 0) {
 			actionBarHelper.setTitle(_window.title);
+		}
 
 		actionBarHelper.setUpAction(() => {
 			_window.close();
