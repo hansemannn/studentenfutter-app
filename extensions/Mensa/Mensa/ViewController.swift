90b6f1ccbb9b53b49f6a7b798182e32f4fdb6ee0
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-08-11 11:34:43 +0200

    Add watchOS extension

---
+++ extensions/Mensa/Mensa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mensa
+//
+//  Created by Hans Knöchel on 02.07.18.
+//  Copyright © 2018 Hans Knöchel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
